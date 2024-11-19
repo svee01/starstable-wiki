76e82a1ad0a55c89ccbc670e7d8112bfed7d8d17
svee01 <s.vaneck2@student.avans.nl>
2024-11-19 21:15:51 +0100

    user entity

--- apps/starstable-wiki/src/app/app.routes.ts
+++ apps/starstable-wiki/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
-import { UserOverviewComponent } from './user/user-overview.component';
-import { UserDetailsComponent } from './user/user-details.component';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 export const appRoutes: Route[] = [
   { path: 'users', component: UserOverviewComponent },
