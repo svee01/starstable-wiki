76f18d67330e135908602541440d3b22b0cf07bb
svee01 <s.vaneck2@student.avans.nl>
2025-04-13 22:14:37 +0200

    smol error

--- apps/data-api/src/app/stable/stable.module.ts
+++ apps/data-api/src/app/stable/stable.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Stable, StableSchema } from './schemas/stable.schema';
 import { StableService } from './stable.service';
 import { StableController } from './stable.controller';
+import { Character, CharacterSchema } from '../character/schemas/character.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Stable.name, schema: StableSchema }])],
+  imports: [MongooseModule.forFeature([
+    { name: Stable.name, schema: StableSchema },
+    { name: Character.name, schema: CharacterSchema },
+  ])],
   providers: [StableService],
   controllers: [StableController],
   exports: [StableService],
